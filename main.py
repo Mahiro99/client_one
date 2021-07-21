249ab7ed7657bc45923715c63a9c10b35dedd5cc
original_hiro <mmahir380@gmail.com>
2021-07-20 21:25:36 -0400

    fetching data

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+import requests
+import json
+
+url = "https://api.opensea.io/api/v1/asset/0x3fe1a4c1481c8351e91b64d5c398b159de07cbc5/5477"
+
+response = requests.request("GET", url)
+final_dictionary = json.loads(response.text)
+print(final_dictionary["traits"])
