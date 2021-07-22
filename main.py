54c5adf46860b2c3d258815ffc0920ff00bafef2
original_hiro <mmahir380@gmail.com>
2021-07-21 22:58:25 -0400

    for loop hell

--- main.py
+++ main.py
@@ -7,145 +7,75 @@ import os
 
 asset_contract_address = "0x3fe1a4c1481c8351e91b64d5c398b159de07cbc5"
 token_id = 5477
-fetchSingleAsset = 'https://api.opensea.io/api/v1/asset/{}/{}'.format(asset_contract_address, token_id)
+fetchSingleAsset = 'https://api.opensea.io/api/v1/asset/{}/{}'.format(
+    asset_contract_address, token_id)
 
 response = requests.request("GET", fetchSingleAsset)
 asset = json.loads(response.text)
-# myDict = {}
-# for sections in asset:
-#     myDict[sections] = asset[sections]
-# print(myDict)
-
 
 wb = Workbook()
 defaultSheet = wb.active
 defaultSheet.title = "assetData"
-ws1 = wb.create_sheet("Mysheet") # insert at the end (default)
-ws2 = wb.create_sheet("Mysheets") # insert at the end (default)
+
+mylist = ['asset_contract', 'collection', 'traits', 'owner', 'creator', 'orders', 'top_ownerships', 'last_sale']
+for sections in asset:
+    if sections in mylist and isinstance(asset[sections], dict):
+        sec = wb.create_sheet(sections)
+        for key, value in asset[sections].items():
+            print(key, '-->', value, "0")
+            if isinstance(value, list):
+                innerSec = wb.create_sheet(sections+"_"+key)
+                for item in value:
+                    for k, v in item.items():
+                        print(k, '-->', v, "1")
+                        innerSec.append([k, v])
+            elif isinstance(value, dict):
+                innerSec = wb.create_sheet(sections+"_"+key)
+                for k, v in value.items():
+                    if isinstance(v, dict):
+                        for m, n in v.items():
+                            if isinstance(n, dict):
+                                for a,b in n.items():
+                                    print(a, '-->', b, "21")
+                                    innerSec.append([a, b])
+                            else:
+                                print(m, '-->', n, "21")
+                                innerSec.append([m, n])
+                    else:
+                        print(k, '-->', v, "22")
+                        innerSec.append([k, v])
+            else:
+                print(key, '-->', value)
+                sec.append([key, str(value)])
+
+    # for indexed data, might wanna save it excel in a diff format instead of key value
+    elif sections in mylist and isinstance(asset[sections], list):
+        sec = wb.create_sheet(sections)
+        for item in asset[sections]:
+            for k, v in item.items():
+                print(k, '-->', v, "3")
+                if isinstance(v, dict):
+                    innerSec = wb.create_sheet(sections+ '_' + k)
+                    for x, y in v.items():
+                        print(x, '-->', y, "WOOO", type(y))
+                        if isinstance(y, dict):
+                            for m, n in y.items():
+                                print(m, '-->', n, "5")
+                                innerSec.append([m, n])
+                        else:
+                            print(x, '-->', y, "6")
+                            innerSec.append([x, y])
+                else:                
+                    print(k, '-->', v, "4")
+                    sec.append([k, v])
+    else:
+        print(type(asset[sections]))
+        print(sections, '-->', asset[sections], "4")
+        defaultSheet.append([sections, str(asset[sections])])
 
 
-
-defaultSheet["A1"] = "hello"
-defaultSheet["B1"] = "world!"
-
-wb.save(filename="hello_world.xlsx")
-
-#asset_contract
-#collection
-    #payment_tokens
-        #indexes with data
-    #primary_asset_contracts
-        #indexes with data
-    #stats
-    #display_data
-#owner
-    #user
-#creator
-    #user
-#traits
-    #indexes with data
-#last_sale
-    #asset
-    #payment_token
-    #transaction
-        #from_account
-            #user
-        #to_account
-            #user
-#orders
-    #index
-        #metadata
-            #asset
-        #maker
-            #user
-        #taker
-            #user
-        #fee_recipient
-            #user
-        #payment_token_contract
-#top_ownerships
-    #index  
-        #owner
-            #user
-
-
-# filename = 'output.xlsx'
-# asset_file = 'assets.xlsx'
-# collection_file = 'collection.xlsx'
-# creator_file = 'creator.xlsx'
-# owner_file = 'owner.xlsx'
-# traits_file = 'traits.xlsx'
-# orders_file = 'orders.xlsx'
-# ownerships_file = 'topownerships.xlsx'
-
-# for x in final_dictionary.values():
-#     if x is None:
-#        x = str('None')
-
-
-# if final_dictionary['last_sale'] is None:
-#     last_sale_price = str('None')
-#     current_price = final_dictionary['orders'][0]['current_price'] 
-#     project_name = final_dictionary['asset_contract']['name']
-#     contract_address = final_dictionary['asset_contract']['address']
-#     token_id = final_dictionary['token_id']
-#     highest_offer = final_dictionary['highest_buyer_commitment']
-#     blockchain = final_dictionary['collection']['payment_tokens'][0]['symbol']
-
-# else:
-#     last_sale_price = final_dictionary['last_sale']['total_price']
-#     current_price = final_dictionary['orders'][0]['current_price'] 
-#     project_name = final_dictionary['asset_contract']['name']
-#     contract_address = final_dictionary['asset_contract']['address']
-#     token_id = final_dictionary['token_id']
-#     highest_offer = final_dictionary['highest_buyer_commitment']
-#     blockchain = final_dictionary['collection']['payment_tokens'][0]['symbol']
-
-
-
-# temp = { 'Project Name' : project_name, 'Token ID': token_id, 
-# 'Contract Address':contract_address,'Last Sale Price':last_sale_price, 
-# 'Current Price':current_price, 'Highest Offer':highest_offer, 'Blockchain': blockchain } 
-
-# print(temp)
-
-# first = 'first.xlsx'
-# df = pd.DataFrame.from_dict(final_dictionary, orient='index')
-
-# df = df.transpose()
-# df.to_excel(first)
-
-# asset = pd.json_normalize(final_dictionary['asset_contract'])
-# asset.to_excel(asset_file)
-
-# collection = pd.json_normalize(final_dictionary['collection'])
-# collection.to_excel(collection_file)
-
-# owner = pd.json_normalize(final_dictionary['owner'])
-# owner.to_excel(owner_file)
-
-# creator = pd.json_normalize(final_dictionary['creator'])
-# creator.to_excel(creator_file)
-
-# traits = pd.json_normalize(final_dictionary['traits'])
-# traits.to_excel(traits_file)
-
-# orders = pd.json_normalize(final_dictionary['orders'])
-# orders.to_excel(orders_file)
-
-# top_ownerships = pd.json_normalize(final_dictionary['top_ownerships'])
-# top_ownerships.to_excel(ownerships_file)
-
-
-# cwd = os.path.abspath('') 
-# files = os.listdir(cwd) 
-
-
-# df2 = pd.DataFrame()
-# for file in files:
-#      if file.endswith('.xlsx'):
-#          df2 = df2.append(pd.read_excel(file), ignore_index=True) 
-
-# df2.head()
-
-# df2.to_excel(filename)
+wb.save(filename="AssetObject.xlsx")
+# [smth, smth, smth, smth, smth, smth
+#     1, 2, 3, 4, 5, 6,
+#     2, 3,4,5,6,7,8,
+# ]
